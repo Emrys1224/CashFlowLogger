932f2b1480b11a282f97aafe546489ebd7d19cac
Emrys1224 <jamerec1224@gmail.com>
2019-04-03 10:40:59 +0800

    - In IncomeDetailsConfirmationFragment, replaced ListView with RecyclerView.

--- app/src/main/java/com/jamerec/cashflowlogger/IncomeDetailsConfirmationFragment.java
+++ app/src/main/java/com/jamerec/cashflowlogger/IncomeDetailsConfirmationFragment.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -25,7 +27,7 @@ public class IncomeDetailsConfirmationFragment extends Fragment
 
     private final static String TAG = "DetailsConfirmFragment";
 
-    private ListView mCategoriesLV;
+    private RecyclerView mCategoriesRV;
     private Button mBtnLog;
     private Button mBtnEdit;
 
@@ -68,18 +70,11 @@ public class IncomeDetailsConfirmationFragment extends Fragment
 
         // Populate allocation list
         mFundList = getArguments().getParcelableArrayList("fundAllocation");
-        FundListAdapter adapter = new FundListAdapter(mContext, mFundList);
-        mCategoriesLV = view.findViewById(R.id.list_funds);
-        mCategoriesLV.setAdapter(adapter);
-
-        // Disable scrolling of list
-        mCategoriesLV.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return (event.getAction() == MotionEvent.ACTION_MOVE);
-            }
-        });
-        setListViewHeightBasedOnChildren(mCategoriesLV);
+        mCategoriesRV = view.findViewById(R.id.list_funds);
+        LinearLayoutManager layoutMgr = new LinearLayoutManager(mContext);
+        mCategoriesRV.setLayoutManager(layoutMgr);
+        FundListAdapterRV adapter = new FundListAdapterRV(mContext, mFundList, null);
+        mCategoriesRV.setAdapter(adapter);
 
         mBtnLog = view.findViewById(R.id.btn_allocate);
         mBtnEdit= view.findViewById(R.id.btn_edit);
@@ -88,30 +83,6 @@ public class IncomeDetailsConfirmationFragment extends Fragment
         mBtnEdit.setOnClickListener(this);
 
         return view;
-    }
-
-    /**** Method for Setting the Height of the ListView dynamically.
-     **** Hack to fix the issue of not showing all the items of the ListView
-     **** when placed inside a ScrollView  ****/
-    public static void setListViewHeightBasedOnChildren(ListView listView) {
-        ListAdapter listAdapter = listView.getAdapter();
-        if (listAdapter == null)
-            return;
-
-        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
-        int totalHeight = 0;
-        View view = null;
-        for (int i = 0; i < listAdapter.getCount(); i++) {
-            view = listAdapter.getView(i, view, listView);
-            if (i == 0)
-                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
-
-            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
-            totalHeight += view.getMeasuredHeight();
-        }
-        ViewGroup.LayoutParams params = listView.getLayoutParams();
-        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
-        listView.setLayoutParams(params);
     }
 
     @Override
